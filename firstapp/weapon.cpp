729c737d7d1797983469c270e771da4804a150a9
SevaShpun <sevashpun@mail.ru>
2022-05-28 00:58:22 +0300

    Добавил класс Weapon

---
+++ firstapp/weapon.cpp
@@ -0,0 +1,36 @@
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Weapon {
+    public:
+        string weaponName = "";
+        char weaponType = ' ';
+        int damage = 0;
+        float distance = 0;
+        Weapon() {}
+
+        Weapon(string weapon, char weaponType, int damage, float distance) {
+            this->weaponName = weapon;
+            this->weaponType = weaponType;
+            this->damage = damage;
+            this->distance = distance;
+        }
+
+        void setValues(string weapon, char weaponType, int damage, float distance) {
+            this->weaponName = weapon;
+            this->weaponType = weaponType;
+            this->damage = damage;
+            this->distance = distance;
+        }
+
+        void print() {
+            cout
+            << this->weaponName << " (" << this->weaponType << "), "
+            << this->damage << " (" << this->distance << ")" << endl;
+        }
+
+        ~Weapon() {}
+};
