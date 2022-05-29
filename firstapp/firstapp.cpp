e7659048af0a14952a00d5ed10bf78c45fbf8aa3
SevaShpun <sevashpun@mail.ru>
2022-05-30 00:24:32 +0300

    Подключил utils

--- firstapp/firstapp.cpp
+++ firstapp/firstapp.cpp
@@ -3,19 +3,29 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
-#include "character.cpp"
+#include <ctime>
+#include "modules/character.cpp"
+#include "modules/utils.cpp"
 
 using namespace std;
 
 int main() {
-    Weapon sword("Sword", 'A', 8, 1.5);;
-    Weapon axe("Axe", 'A', 8, 1);
+    srand(time(NULL));
+    int sword_rand_dmg = rand_int(0, 8);
+    int axe_rand_dmg = rand_int(0, 6);
+
+    Weapon sword;
+    sword.weaponName = "Sword";
+    sword.weaponType = 'A';
+    sword.damage = sword_rand_dmg;
+    Weapon axe("Axe", 'A', 6);
 
     Character player_1("Dev", "Paladin", sword, 100);
     player_1.print();
     
     Character player_2("Barbar", "Ogre", axe, 100);
     player_2.print();
+
     return 0;
 }
 // clear; g++ -o Debug/firstapp firstapp.cpp; ./Debug/firstapp.exe
